d02c2d87064618454744fc93ae95034affa7a894
baranyizs <149656878+baranyizs@users.noreply.github.com>
2024-04-16 22:08:51 +0200

    Create 2.2.v

---
+++ 2.2.v
@@ -0,0 +1,8 @@
+Lemma problem_2 : forall A B C : Prop, ((B -> A) /\ (C -> A)) -> ((B \/ C) -> A).
+Proof.
+intros A B C H.
+destruct H as [HBA HCA].
+intros [HB | HC].
+- apply HBA. assumption.
+- apply HCA. assumption.
+Qed.
